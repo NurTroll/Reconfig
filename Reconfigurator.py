a1630e2cde154e5ef87ffea2ce5084be1144b0cb
NurTroll <furicirulol@gmail.com>
2023-11-19 21:45:52 +0300

    Добавил описание программы

--- Reconfigurator.py
+++ Reconfigurator.py
@@ -1,8 +1,12 @@
+from datetime import date #импорт модуля для определения текущей даты
+import os #импорт модуля для работы с папками в оси, для создания папки
+#Функция возвращающая hostname коммутатора
 def get_sysname(text):
     for line in text:
         if 'sysname' in line:
             return line.split()[1]
 
+#Функция возвращающая все вланы используемые на коммутаторе
 def get_vlans(text):
     vlans = list()
     for i in range(len(text)):
@@ -11,19 +15,19 @@ def get_vlans(text):
                 vlans.append(text[i + 1].split()[1])
     return vlans
 
-
+#Функция возвращаюзая ip адрес и маску коммутатора
 def get_ip(text):
     for line in text:
         if 'ip address 10.' in line:
             return line.strip()
 
-
+#Функция возвращаюзая шлюз
 def get_gw(text):
     for line in text:
         if 'ip route-static' in line:
             return line.split()[4]
 
-
+#Функция возвращающая описание портов
 def get_description(text):
     descriptions = list()
     for i in range(len(text)):
@@ -34,12 +38,14 @@ def get_description(text):
                 descriptions.append(text[i + 1].strip().replace('"', '')[12:])
     return descriptions
 
-
+#Функция возвращающая настройки для портов.
 def get_port_status(text):
     port_status = [[], []]
     for i in range(len(text)):
         if text[i].startswith('interface Ethernet'):
             port_status[0].append(text[i].strip()[18:])
+            if text[i + 1].startswith(' description') == False:
+                i -= 1
             if text[i + 2].startswith(" shutdown"):
                 port_status[1].append(text[i + 2].strip())
             elif text[i + 2].startswith(" port link-type access"):
@@ -57,7 +63,7 @@ def get_port_status(text):
                         port_status[1].append("shutdown")
     return port_status
 
-
+#Создаёи описани вланов
 def get_vlans_names(ls):
     s = ''
     for i in range(1, len(ls), 2):
@@ -75,11 +81,13 @@ print(sys_name)
 print(ip, gw)
 description_list = get_description(conf_text)
 vlans_list = get_vlans(conf_text)
-management = ''.join([vlans_list[i - 1] for i in range(0, len(vlans_list)) if vlans_list[i] == "management"])
-iptv = ''.join([vlans_list[i - 1] for i in range(0, len(vlans_list)) if vlans_list[i] == "IPTV"])
+management = ''.join([vlans_list[i - 1] for i in range(0, len(vlans_list)) if vlans_list[i] == "management" or vlans_list[i] == "managament" ]) #находим влан управления
+print(management)
+iptv = ''.join([vlans_list[i - 1] for i in range(0, len(vlans_list)) if vlans_list[i] == "IPTV"]) #находим iptv влан
 vlans_names = get_vlans_names(vlans_list)
 all_vlans = ",".join(vlans_list[0::2])
 port_status = get_port_status(conf_text)
+#заменяем статус портов на соответствующий для коммутатора eltex2424p
 for i in range(len(port_status[1])):
     x = ''
     ls = []
@@ -100,7 +108,7 @@ for i in range(len(port_status[1])):
 
 print(vlans_list)
 print(description_list)
-
+date = 'backup/' + str(date.today())
 empty_config = open('MES2424.txt', 'r', encoding='utf-8')
 conf_text = empty_config.read()
 new_text = conf_text.replace('<hostname>', sys_name)
@@ -115,8 +123,17 @@ for el in range(0,len(description_list)):
 for ports in range(len(port_status[1])):
     new_text = new_text.replace('<status>', port_status[1][ports], 1)
 filename = 'startup.conf'
-output = open(filename, 'w', encoding='utf-8')
+tftp = open('tftp.txt', 'r', encoding='utf-8')
+#Берётся путь из файла tftp.txt для сохранения готовой конфигурации в папке по данному пути
+tftp_wey = tftp.readlines()[0].strip()
+tftp.close()
+output = open(f'{tftp_wey}{filename}', 'w', encoding='utf-8')
 print(new_text, file=output)
+#Проверяем есть ли папка для бекапа на сегодняшнуюю дату, если нет создаём её
+if os.path.exists(date) == False:
+    os.mkdir(date)
+output2 = open(f'{date}/{sys_name}.conf', 'w', encoding='utf-8')
+print(new_text, file=output2)
 empty_config.close()
 output.close()
 
